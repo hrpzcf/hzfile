ae001b45e8e8b429cd030c7bb972c486a6e863b0
hrpzcf <hrpzcf@foxmail.com>
2021-11-06 11:08:41 +0800

    update website

--- hzfile/__init__.py
+++ hzfile/__init__.py
@@ -7,14 +7,6 @@ VERNUM = 0, 0, 1
 VERSION = ".".join(map(str, VERNUM))
 AUTHOR = "hrpzcf"
 EMAIL = "hrpzcf@foxmail.com"
-WEBSITE = "https://gitee.com/hrpzcf/hzfile"
+WEBSITE = "https://github.com/hrpzcf/hzfile"
 
-__all__ = [
-    "HzFile",
-    "NAME",
-    "VERNUM",
-    "VERSION",
-    "AUTHOR",
-    "EMAIL",
-    "WEBSITE",
-]
+__all__ = ["HzFile", "NAME", "VERNUM", "VERSION", "AUTHOR", "EMAIL", "WEBSITE"]
